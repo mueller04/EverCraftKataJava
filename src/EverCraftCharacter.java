e3f226144746fbfbe353afa8dda2442f4dac59f1
Mike Mueller <mikemueller04@hotmail.com>
2016-02-18 10:14:33 -0500

    passed canGetCharacterName

---
+++ src/EverCraftCharacter.java
@@ -0,0 +1,13 @@
+
+public class EverCraftCharacter {
+
+    String name;
+
+    public EverCraftCharacter(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
