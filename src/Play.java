fce61cf5c44d9b992af7153679f02b1d3c348029
Mike Mueller <mikemueller04@hotmail.com>
2016-02-20 13:45:31 -0500

    refactored play

--- src/Play.java
+++ src/Play.java
@@ -8,25 +8,29 @@ public class Play {
 
         if (rollNumber >= defendingCharacter.getArmor()){
 
-            int strengthScore = attackingCharacter.getAbilities().getStrengthScore();
-            int hitPointsToDeduce = attackingCharacter.getAbilities().getStrengthModifier(strengthScore);
-
-            if (hitPointsToDeduce < 1) {
-                hitPointsToDeduce = 1;
-            }
-
-            if (rollNumber == 20) {
-                defendingCharacter.setHitPoints(defendingCharacter.getHitPoints() - hitPointsToDeduce * 2);
-            } else {
-
-                defendingCharacter.setHitPoints(defendingCharacter.getHitPoints() - hitPointsToDeduce);
-            }
-
+            reducedefendingCharacterHitPoints(defendingCharacter, attackingCharacter, rollNumber);
             defendingCharacter.updateLifeStatus();
-
             return "it's a hit";
+
         } else {
             return "attack glanced off the armor";
+        }
+    }
+
+    private void reducedefendingCharacterHitPoints(EverCraftCharacter defendingCharacter, EverCraftCharacter attackingCharacter, int rollNumber) {
+
+        int strengthScore = attackingCharacter.getAbilities().getStrengthScore();
+        int amountToReduceHitPointsBy = attackingCharacter.getAbilities().getStrengthModifier(strengthScore);
+
+        if (amountToReduceHitPointsBy < 1) {
+            amountToReduceHitPointsBy = 1;
+        }
+
+        if (rollNumber == 20) {
+            defendingCharacter.setHitPoints(defendingCharacter.getHitPoints() - amountToReduceHitPointsBy * 2);
+        } else {
+
+            defendingCharacter.setHitPoints(defendingCharacter.getHitPoints() - amountToReduceHitPointsBy);
         }
     }
 
